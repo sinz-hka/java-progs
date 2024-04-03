9c73cf54730eca5465ecded66638d79d7f78a13d
Carsten Sinz <carsten.sinz@h-ka.de>
2024-04-03 17:33:37 +0200

    added sample programs for K2

---
+++ K2/DataTypeDemo.java
@@ -0,0 +1,10 @@
+class DataTypeDemo {
+    public static void main(String[] args) {
+        double a = 2.5, b = -5.1, c;
+        c = a + b;
+        System.out.println("c = " + c);
+        byte x = 100, y = 110;
+	int z = x + y;
+        System.out.println("x + y = " + z);
+    }
+}
