36a54787a14be91bf1521693afa33a3ada5110b3
Carsten Sinz <carsten.sinz@me.com>
2025-06-15 15:38:14 +0200

    Update Merge-Sort: remove unnecessary comments

--- src/K10_Recursion/MergeSort/Main.java
+++ src/K10_Recursion/MergeSort/Main.java
@@ -7,13 +7,10 @@ public class Main {
 
         int[] a = { 7, 9, 3, 2, 1, 4, -8, 10, 1, 17 };
 
-        // print original array
         System.out.println("Original array: " + Arrays.toString(a));
 
-        // merge sort
         Sorting.sort(a);
 
-        // print result
         System.out.println("Sorted array: " + Arrays.toString(a));
     }
 }
